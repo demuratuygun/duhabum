48aa19b93a88d3194a6a4e2243331e082a9ae41b
demuratuygun <de.muratuygun@gmail.com>
2024-08-08 10:27:12 +0300

    payment api entegration installemt added

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,11 @@ export default function Home() {
       <div style={{ position: "fixed", top: "-200px", left: "calc(25vw - 400px)", rotate: "-12deg", zIndex: 10 }}><Light/></div>
       <div style={{ position: "fixed", top: '0px', right: "0px", width: "100vw", margin: "0px", zIndex: 1000 }}><Header/></div>
       <div style={{ position: "fixed", right: 20, top: 30, zIndex:999 }}><Logo type=''/></div>
-      
+      <Whatsapp />
       
       <div style={{ position: "absolute", top: 0, left:0, display: 'flex', flexDirection: 'column', gap: "200px", width: '100%', overflow: 'clip' }}>
         
-        <Whatsapp />
+        
         <IntroFrame />
         <MessageFrame />
         <Transition />
