6aff1f8c24ad9940fb5440ac15510c90002e9e4e
demuratuygun <de.muratuygun@gmail.com>
2024-08-16 17:12:06 +0300

    generate apyment discount bug fix

--- app/package/[packageId]/[month]/page.tsx
+++ app/package/[packageId]/[month]/page.tsx
@@ -82,6 +82,16 @@ export default function Form({params}:{params:{packageId:number, month:string}})
   }, [])
   
 
+  /*
+            <AskText setObject={( theList, direction) => setObject( theList, direction) }
+            key="name" question="iletişim bilgileri"
+            entries={[
+              { value: data["name"]??"", key:"name", example:[ 'isim soyisim', 'duha duman'], verify:"" },
+              { value: data["email"]??"", key:"email", example:[ 'eposta girin', 'adsoyad@gmail.com'], verify:"" },
+              { value: data["phone"]??"", key:"phone", example:[ 'telefon girin', '0 555 555 55 55'], verify:"" }
+            ]} 
+          />,
+  */
 
   const questions = [
           <MakeOffer plan={packages.tr[params.packageId]} months={parseInt(params.month)} setObject={( theList, direction) => setObject( theList, direction)}/>,
