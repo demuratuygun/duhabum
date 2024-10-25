994e6e1365b51d0a2219ea33e2c223dbef34db2e
demuratuygun <de.muratuygun@gmail.com>
2024-10-25 19:26:07 +0300

    Remove sensitive data

--- app/api/record/route.js
+++ app/api/record/route.js
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { google } from 'googleapis';
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
 
 // Create a JWT client
 const auth = new google.auth.JWT(
